5761b870242b5782dcf61911536d166313b75dea
Vinayak <vinayak1998th@Gmail.com>
2018-12-02 17:48:33 +0530

    Added Views

--- main/views.py
+++ main/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here
+
+def index(request):
+    return render(request,'templates/index.html',{})
+def output(request):
+    return render(request,'templates/output.html',{})
