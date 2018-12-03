258e3e205da2a3c6885964051f183668c0f6f321
samirashoi <issamrasheed@gmail.com>
2018-12-03 08:56:46 +0530

    Clean up code
    Remove .vscode/ files
    
    Clean up code

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from django.conf.urls import url
 from . import views
 
+
 urlpatterns = [
-    path('/output/', views.summariser, name='summariser'),
+    path('output/', views.summariser, name='summariser'),
     path('', views.index, name='index'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,19 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .Newsletterapi import *
-from .forms import newssite
-from django.shortcuts import redirect
-# Create your views here
+from .forms import WebsiteForm
+
 
 def index(request):
     trends = trending()
     if request.method == 'POST':
-        form = newssite(request.POST)
+        form = WebsiteForm(request.POST)
         if form.is_valid():
             request.session['weblink'] = form.cleaned_data['weblink']
             return redirect('summariser')
-    form = newssite()
+    form = WebsiteForm()
     return render(request,'main/index.html',{'trending_terms':trends[0],'trending_urls':trends[1],'form': form})
+
+    
 def summariser(request):
     newsurl = request.session.get('weblink')
-    text = get_text(newsurl)
-    return render(request,'main/output.html',{'text':text})
+    text, text_summary = get_summary(newsurl)
+    return render(request,'main/output.html',{'text':text, 'text_summary':text_summary})
