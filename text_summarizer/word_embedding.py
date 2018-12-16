280410413757b7ad3a333981d4677555bfe84bac
Nikil99 <nikilalakunta09031999@gmail.com>
2018-12-17 01:40:51 +0530

    Add files via upload (#6)
    * Add files via upload
    
    * Update word_embedding.py

---
+++ text_summarizer/word_embedding.py
@@ -0,0 +1,36 @@
+from allennlp.data.dataset import Batch
+from allennlp.data.fields import TextField
+from allennlp.data.instance import Instance
+from allennlp.data.token_indexers.wordpiece_indexer import PretrainedBertIndexer
+from allennlp.data.tokenizers import WordTokenizer
+from allennlp.data.tokenizers.word_splitter import BertBasicWordSplitter
+from allennlp.data.vocabulary import Vocabulary
+from allennlp.modules.token_embedders.bert_token_embedder import PretrainedBertEmbedder
+import re
+
+class preprocessing(object):
+  
+    def __init__(self,text):
+        self.text=text
+    def word_embeddings(self):
+        words = re.split(r'\W+',self.text) 
+        Text = ' '.join(words)
+        
+        tokenizer=WordTokenizer(word_splitter=BertBasicWordSplitter())
+        
+        tokens = tokenizer.tokenize(Text)
+        vocab = Vocabulary()
+        token_indexer = PretrainedBertIndexer('bert-base-uncased')
+        
+        instance = Instance({"tokens":TextField(tokens,{'bert':token_indexer})})
+        batch = Batch([instance])
+        batch.index_instances(vocab)
+        
+        padding_lenghts = batch.get_padding_lengths()
+        tensor_dict = batch.as_tensor_dict(padding_lenghts)
+        
+        Tokens = tensor_dict["tokens"]
+        
+        model = PretrainedBertEmbedder('bert-base-uncased')
+        bert_vectors = model(Tokens["bert"])
+        return(bert_vectors)
