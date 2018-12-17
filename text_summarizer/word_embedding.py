3a6f0082b9720c88e7fbcb08ff5e8d55c3707c2b
Pratik Savla <pratiksavla07@gmail.com>
2018-12-17 09:31:25 +0530

    Update word_embedding.py
    Previous version had to load vocabulary, pretrained bert indexer and embedder everytime function was called which was creating significant delay while being used. Moved those file loading lines outside the class thus making it more efficient.

--- text_summarizer/word_embedding.py
+++ text_summarizer/word_embedding.py
@@ -8,6 +8,11 @@ from allennlp.data.vocabulary import Vocabulary
 from allennlp.modules.token_embedders.bert_token_embedder import PretrainedBertEmbedder
 import re
 
+tokenizer = WordTokenizer(word_splitter=BertBasicWordSplitter())
+vocab = Vocabulary()
+token_indexer = PretrainedBertIndexer('bert-base-uncased')
+model = PretrainedBertEmbedder('bert-base-uncased')
+
 class preprocessing(object):
   
     def __init__(self,text):
@@ -16,11 +21,7 @@ class preprocessing(object):
         words = re.split(r'\W+',self.text) 
         Text = ' '.join(words)
         
-        tokenizer=WordTokenizer(word_splitter=BertBasicWordSplitter())
-        
         tokens = tokenizer.tokenize(Text)
-        vocab = Vocabulary()
-        token_indexer = PretrainedBertIndexer('bert-base-uncased')
         
         instance = Instance({"tokens":TextField(tokens,{'bert':token_indexer})})
         batch = Batch([instance])
@@ -31,6 +32,5 @@ class preprocessing(object):
         
         Tokens = tensor_dict["tokens"]
         
-        model = PretrainedBertEmbedder('bert-base-uncased')
         bert_vectors = model(Tokens["bert"])
         return(bert_vectors)
