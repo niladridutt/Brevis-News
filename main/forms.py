1c528c104e995b327575e56f9f8de2db38df4962
Vinayak <vinayak1998th@Gmail.com>
2018-12-20 17:26:04 +0530

    Comments added

--- main/forms.py
+++ main/forms.py
@@ -1,4 +1,4 @@
 from django import forms
-
+""" Class based form model to extend usage to add ability to save summaries into database"""
 class WebsiteForm(forms.Form):
     weblink = forms.URLField(label = 'Paste url here (with https)')
